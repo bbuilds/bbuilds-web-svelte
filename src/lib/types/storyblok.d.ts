dd02d19712a95d88d37d6f557dd06ebcfd702b08
Branden <hello@brandenbuilds.com>
2026-05-27 16:52:16 -0500

    chore: seo posts blocks

--- src/lib/types/storyblok.d.ts
+++ src/lib/types/storyblok.d.ts
@@ -141,6 +141,7 @@ export interface StoryblokBlogPost {
 	summary?: string;
 	Category?: (number | string)[];
 	content?: StoryblokRichtext;
+	seo?: StoryblokSEO[];
 	component: 'Blog Post';
 	_uid: string;
 }
